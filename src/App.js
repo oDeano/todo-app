d8ed15fdfc1e820703db676ea5a23eca77fb75ef
Dean Cropton <dean.cropton2@outlook.com>
2022-03-10 11:15:56 +0000

    missed changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Display from "./components/Display";
+import TaskList from "./components/TaskList";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  const addTask = (newTask) => {
+    setTaskList([...taskList, newTask]);
+    console.log(taskList);
+  };
+
   return (
     <div className="App">
-      <div></div>
+      <Header />
+      <div className="content">
+        <div className="nav">
+          <Nav />
+        </div>
+        <div className="display">
+          <Display onSubmit={addTask} />
+          <TaskList taskList={taskList} />
+        </div>
+      </div>
     </div>
   );
 }
