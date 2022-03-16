913c7b6c78cc539511b04c209e58e83a03ca1666
Dean Cropton <dean.cropton2@outlook.com>
2022-03-16 10:10:54 +0000

    Can now toggle between incomplete/all tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,70 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import Task from "./Task";
 
 const TaskList = ({ tasks, handleCheck, handleUpdateTask }) => {
+  const [complete, setComplete] = useState(false);
+  const [buttonText, setButtonText] = useState("Incomplete tasks");
+  const [buttonVar, setButtonVar] = useState("danger");
+
+  const toggleComplete = () => setComplete((prev) => !prev);
+
+  function toggleButtonText() {
+    if (complete === false) {
+      setButtonText("Incomplete tasks");
+    } else {
+      setButtonText("All tasks");
+    }
+  }
+
+  function toggleButtonVar() {
+    if (complete === false) {
+      setButtonVar("danger");
+    } else {
+      setButtonVar("success");
+    }
+  }
+
+  function handleBtnToggle() {
+    toggleComplete();
+  }
+
+  useEffect(() => {
+    toggleButtonText();
+    toggleButtonVar();
+  }, [complete]);
+
   return (
     <>
-      <div>Tasks</div>
+      <Button variant={`outline-${buttonVar}`} onClick={handleBtnToggle}>
+        {buttonText}
+      </Button>
       <ul>
-        {tasks.map((task) => {
+        {complete ? (
+          <ShowAll
+            tasks={tasks}
+            handleCheck={handleCheck}
+            handleUpdateTask={handleUpdateTask}
+          />
+        ) : (
+          <ShowIncomplete
+            tasks={tasks}
+            handleCheck={handleCheck}
+            handleUpdateTask={handleUpdateTask}
+          />
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default TaskList;
+
+const ShowIncomplete = ({ tasks, handleCheck, handleUpdateTask }) => {
+  return (
+    <>
+      {tasks.map((task) => {
+        if (task.isComplete === false) {
           return (
             <Task
               task={task}
@@ -14,10 +73,25 @@ const TaskList = ({ tasks, handleCheck, handleUpdateTask }) => {
               handleUpdateTask={handleUpdateTask}
             />
           );
-        })}
-      </ul>
+        }
+      })}
     </>
   );
 };
 
-export default TaskList;
+const ShowAll = ({ tasks, handleCheck, handleUpdateTask }) => {
+  return (
+    <>
+      {tasks.map((task) => {
+        return (
+          <Task
+            task={task}
+            key={task.taskId}
+            handleCheck={handleCheck}
+            handleUpdateTask={handleUpdateTask}
+          />
+        );
+      })}
+    </>
+  );
+};
